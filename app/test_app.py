200b7248129a17a601daeb7042c251dc0e89e758
Tholiwe Mchunu <mchunutholiwe@gmail.com>
2026-08-13 08:17:54 +0200

    Initial CI/CD application

---
+++ app/test_app.py
@@ -0,0 +1,19 @@
+from app.app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
+def test_version():
+    client = app.test_client()
+    response = client.get("/version")
+    assert response.status_code == 200
