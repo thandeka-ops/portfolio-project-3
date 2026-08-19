454cf521df79d25f884ab55854b26adc983e4a9c
Tholiwe Mchunu <mchunutholiwe@gmail.com>
2026-08-19 15:05:12 +0200

    Release version 3.0.2

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def home():
     return jsonify({
         "application": "CI/CD Demo Application",
         "status": "running",
-        "version": "3.0.1"
+        "version": "3.0.2"
     })
 
 
@@ -22,7 +22,7 @@ def health():
 @app.route("/version")
 def version():
     return jsonify({
-        "version": "3.0.1"
+        "version": "3.0.2"
     })
 
 
